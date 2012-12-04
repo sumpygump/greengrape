32e19fbff594221c57387881c6578b38f894b7c6
Jansen Price <sumpygump@gmail.com>
2012-12-04 05:34:39 -0600

    - Added test for Theme class

--- tests/lib/Greengrape/View/ThemeTest.php
+++ tests/lib/Greengrape/View/ThemeTest.php
@@ -8,6 +8,7 @@
 namespace Greengrape\Tests\View;
 
 use Greengrape\View\Theme;
+use Greengrape\View\AssetManager;
 
 /**
  * Theme Test
@@ -25,7 +26,9 @@ class ThemeTest extends \BaseTestCase
      */
     public function setUp()
     {
-        //$this->_object = new Theme();
+        mkdir(APP_PATH . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'faketheme');
+
+        $this->_object = new Theme('faketheme', '/baseUrl', APP_PATH . DIRECTORY_SEPARATOR . 'tests');
     }
 
     /**
@@ -35,6 +38,7 @@ class ThemeTest extends \BaseTestCase
      */
     public function tearDown()
     {
+        rmdir(APP_PATH . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'faketheme');
     }
 
     /**
@@ -46,5 +50,74 @@ class ThemeTest extends \BaseTestCase
     public function testConstructNoArgs()
     {
         $theme = new Theme();
+    }
+
+    /**
+     * testConstructDefaultDir
+     *
+     * @expectedException Greengrape\Exception\NotFoundException
+     * @return void
+     */
+    public function testConstructDefaultDir()
+    {
+        $theme = new Theme('foobar', '/baseUrl');
+    }
+
+    /**
+     * testConstructCustomDir
+     *
+     * @expectedException Greengrape\Exception\NotFoundException
+     * @return void
+     */
+    public function testConstructCustomDirNoExist()
+    {
+        $theme = new Theme('foobar', '/baseUrl', APP_PATH . DIRECTORY_SEPARATOR . 'tests');
+    }
+
+    public function testConstructCustomDirExist()
+    {
+        $themePath = APP_PATH . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'newfaketheme';
+        mkdir($themePath);
+
+        $theme = new Theme('newfaketheme', '/baseUrl', APP_PATH . DIRECTORY_SEPARATOR . 'tests');
+
+        rmdir($themePath);
+    }
+
+    public function testGetAssetManager()
+    {
+        $assetManager = $this->_object->getAssetManager();
+
+        $this->assertTrue($assetManager instanceof AssetManager);
+    }
+
+    public function testGetPathBlank()
+    {
+        $path = $this->_object->getPath();
+
+        $expected = APP_PATH . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'faketheme';
+        $this->assertEquals($expected, $path);
+    }
+
+    public function testGetPathFile()
+    {
+        $path = $this->_object->getPath('foobar');
+
+        $expected = APP_PATH . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'faketheme'
+            . DIRECTORY_SEPARATOR . 'foobar';
+
+        $this->assertEquals($expected, $path);
+    }
+
+    public function testSetDefaultTitle()
+    {
+        $this->_object->setDefaultTitle('wonky wonk');
+
+        $this->assertEquals('wonky wonk', $this->_object->getDefaultTitle());
+    }
+
+    public function testGetDefaultTitleWhenNotSet()
+    {
+        $this->assertEquals('[Greengrape]', $this->_object->getDefaultTitle());
     }
 }
